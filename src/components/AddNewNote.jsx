996c490ff0bbd229e3ce79693626952e08804d59
hossein rahmati <rmthossein@gmail.com>
2023-09-21 18:31:56 +0330

    fix some bug

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import toast from "react-hot-toast";
-import "react-toastify/dist/ReactToastify.css";
 
 function AddNewNote({ onAddNote, translate, theme }) {
   const [title, setTitle] = useState("");
